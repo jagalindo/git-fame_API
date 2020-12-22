a631b17e0a33e6af50a9c00d8e58a17ca6084315
José A. Galindo <jagalindo@pop-os.localdomain>
2020-12-22 23:43:25 +0100

    chrome extensino working

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,9 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('clickIt');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        alert("Hello..! It's my first chrome extension.");
-      });
-    }, false);
-  }, false);
+  chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+  function(tabs){
+    fetch("https://immense-dusk-28515.herokuapp.com/exec_fame?url="+tabs[0].url).then(r => r.text()).then(result => {
+      document.getElementById("placeholder").innerHTML=result
+    })
+
+  });  
